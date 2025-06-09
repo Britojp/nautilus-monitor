0e84846a4b2d5a41a1ad1c452c828ab37db4d1d3
jaimejcs <jaimesilva1190@gmail.com>
2025-06-08 22:40:19 -0300

    feat: Novos campos em Alerta

--- backend/models/models.py
+++ backend/models/models.py
@@ -2,15 +2,35 @@ from pydantic import BaseModel, Field
 from datetime import datetime, timezone
 from typing import Optional
 
+"""
+Estrutura dos dados da leitura do sensor
+
+Setor (str) : Setor onde se encontra o sensor
+
+Temperatura (float) : Temperatura medida no componente elétrico da infraestrutura expressa em °C
+
+Umidade (float) : Umidade relativa encontrada no ambiente expressa em %
+
+Voltagem (float) : Voltagem medida no componente elétrico da infraestrutura expressa em kV
+
+Corrente (float) : Corrente elétrica que passa no componente elétrico expressa em kA
+
+Impedância (float) : Magnitude da impedância no componente elétrico expressa em Ohms
+"""
 class SensorLeitura(BaseModel):
     setor:str
     temperatura:float
     umidade:float
     voltagem:float
     corrente:float
-    impedancia:float
+    impedancia:complex
     timestamp: Optional[datetime] = Field(default_factory=datetime.now(timezone.utc))
 
+"""
+Estrutura dos dados de definição dos limites
+
+Valores máximo e mínimo para cada atributo de SensorLeitura
+"""
 class Limites(BaseModel):
     temperatura_min: float
     temperatura_max: float
@@ -20,9 +40,15 @@ class Limites(BaseModel):
     voltagem_max: float
     corrente_min: float
     corrente_max: float
-    impedancia_min: float
-    impedancia_max: float
+    impedancia_min: complex
+    impedancia_max: complex
 
+
+"""
+Estrutura dos dados de alerta
+"""
 class Alerta(BaseModel):
     mensagem: str
     timestamp: datetime = Field(default_factory=lambda : datetime.now(timezone.utc))
+    codigo: int
+    severidade: str
