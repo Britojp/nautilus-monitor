865c05f4cbd7976297017fe5dd8348b7e0699614
jaimejcs <jaimesilva1190@gmail.com>
2025-06-09 21:59:55 -0300

    feat: mudanças na rota /sensor-data

--- backend/app.py
+++ backend/app.py
@@ -40,14 +40,19 @@ def get_users():
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
 
-# Gera uma leitura com valores aleatórios para simular o sensor
+# Gera uma leitura com valores aleatórios para simular o sensor, enviando os alertas se gerados
 @app.route('/sensor-data', methods=['GET'])
 def simular_leitura():
     sensor = Sensor(setor=setores[randint(0,5)])
     leitura = sensor.gerar_leitura()
-    db.dados.insert_one(leitura.dict)
-    verificar_limites(leitura)
-    return jsonify(leitura.dict)
+    db.leitura_sensores.insert_one(leitura.dict())
+
+    alertas = verificar_limites(leitura)
+
+    return jsonify({
+        "leitura": leitura.dict(),
+        "alertas": alertas
+    })
 
 # Últimas 50 leituras
 @app.route('/sensor-data/historico', methods=['GET'])
@@ -61,21 +66,29 @@ def historico_leituras():
 
     return jsonify(leituras)
 
-def verificar_limites(dado: SensorLeitura):
-    for var in limites:
-        valor = getattr(dado, var)
-        
-        min_, max_ = limites[var]
+def verificar_limites(dado: SensorLeitura) -> list:
+    alertas_gerados = []
+
+    for var in limites.dict():
+        base = var.split("_")[0]
+        if f"{base}_min" not in limites.dict():
+            continue
+
+        min_ = getattr(limites, f"{base}_min")
+        max_ = getattr(limites, f"{base}_max")
+        valor = getattr(dado, base)
 
         if not (min_ <= valor <= max_):
-            
             sev = calcular_severidade(valor, min_, max_)
+            cod = codigo_alerta(base)
+            msg = f"{base.capitalize()} fora do limite: {valor} no setor {dado.setor}"
 
-            cod = codigo_alerta(var)
-
-            msg = f"{var.capitalize()} fora do limite: {valor} no setor {dado.setor}"
             alerta = Alerta(mensagem=msg, codigo=cod, severidade=sev)
             db.alertas.insert_one(alerta.dict())
+            alertas_gerados.append(alerta.dict())
+
+    return alertas_gerados
+
 
 # Retorna o limite global
 @app.route('/limites', methods=['GET'])
